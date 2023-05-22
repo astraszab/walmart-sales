737442efa8c6252ba860a4aa33ab637b7e3a0153
astraszab <uladzislau_astrashab@epam.com>
2023-05-22 12:00:18 +0300

    process raw data

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Installation instructions
 - Clone the repo
-- Download the [dataset](https://www.kaggle.com/competitions/walmart-recruiting-sales-in-stormy-weather/) and put it into data/raw
+- Download the [dataset](https://www.kaggle.com/competitions/walmart-recruiting-sales-in-stormy-weather/) and put the archive into data/raw. No need to extract it
 - Install Poetry as described [here](https://python-poetry.org/docs/#installation)
 - Install Python 3.9 and make sure it's active in the repo root directory by running ```python --version```. I'd recommend using [pyenv](https://github.com/pyenv/pyenv) for this
 - Run ```poetry install```
+- Run ```poetry run process-raw-data```
---
+++ src/walmart_sales/data/process_raw_data.py
@@ -0,0 +1,31 @@
+"""Process raw data."""
+
+import zipfile
+
+
+import click
+import pandas as pd
+
+
+@click.command()
+def process_raw_data():
+    """Read raw data, merge, and save the result."""
+    with zipfile.ZipFile(
+        "data/raw/walmart-recruiting-sales-in-stormy-weather.zip", "r"
+    ) as zip_ref:
+        zip_ref.extractall("data/interim")
+    df_train = pd.read_csv("data/interim/train.csv.zip")
+    df_weather = pd.read_csv("data/interim/weather.csv.zip")
+    df_key = pd.read_csv("data/interim/key.csv.zip")
+    click.echo("Merging tables...")
+    df_merged = df_train.merge(df_key).merge(df_weather)
+    click.echo("Removing items with zero sales...")
+    df_processed = df_merged[
+        df_merged.groupby(["item_nbr", "store_nbr"], as_index=False)["units"].transform(
+            "sum"
+        )
+        != 0
+    ]
+    df_processed.to_csv("data/processed/df_full.csv")
+    click.secho("Extracted data to data/interim/", fg="green")
+    click.secho("Saved merged data to data/processed/df_full.csv", fg="green")
