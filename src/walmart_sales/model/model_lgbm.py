4eda83bfc5606ecf6150b58fbe85cb469d811e8e
astraszab <uladzislau_astrashab@epam.com>
2023-05-26 17:44:00 +0300

    use target differentiation and clipping

--- src/walmart_sales/model/model_lgbm.py
+++ src/walmart_sales/model/model_lgbm.py
@@ -3,6 +3,7 @@
 from typing import Any
 
 from lightgbm import LGBMRegressor
+import numpy as np
 import pandas as pd
 
 from .model_base import ModelBase
@@ -16,18 +17,28 @@ class WalmartLGBM(ModelBase):
         self,
         lgbm_regressor: LGBMRegressor,
         transfromer: WalmartDataTransformer,
+        differentiate_target: bool = True,
+        clip_foreacst: bool = True,
     ):
         """Initialize a model."""
         super().__init__()
         self._lgbm = lgbm_regressor
         self._transformer = transfromer
+        self._differentiate_target = differentiate_target
+        self._clip_forecast = clip_foreacst
 
     def _fit(self, df: pd.DataFrame) -> "WalmartLGBM":
         X, y = self._transformer.fit_transform(df)
+        if self._differentiate_target:
+            y = y - X["lag_1"]
         self._lgbm = self._lgbm.fit(X, y)
         return self
 
     def _predict(self, df: pd.DataFrame) -> "pd.Series[Any]":
         X = self._transformer.transform(df)
         preds = self._lgbm.predict(X)
+        if self._differentiate_target:
+            preds = preds + X["lag_1"]
+        if self._clip_forecast:
+            preds = np.clip(preds, 0, None)
         return pd.Series(preds, index=df.index)
